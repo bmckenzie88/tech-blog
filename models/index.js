3d07db03f2f6cd53b94f786b8a7b87b0f1706b09
bryce <brycelmckenzie@gmail.com>
2022-11-30 21:46:08 -0800

    comment and post models created

---
+++ models/index.js
@@ -0,0 +1,26 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+
+User.hasMany(Post, {
+  foreignKey: 'post_creator',
+  onDelete: 'CASCADE'
+});
+User.hasMany(Comment, {
+  foreignKey: 'comment_creator',
+  onDelete: 'CASCADE'
+});
+
+
+Post.belongsTo(User, {
+  foreignKey: 'post_creator'
+});
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment };
